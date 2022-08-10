d4aad52c809b976642bd8fe2a8bf362a194aa943
0378570940 <0378570940>
2022-08-10 18:02:42 +0700

    [Thực hành] Nhúng JavaScript

---
+++ luyện tập/biến kiểu dữu liệu/index.js
@@ -0,0 +1,12 @@
+// var intros= "Hello Việt";
+//     document.getElementById('intro').innerHTML= intros;
+
+
+function myFunction() {
+    // document.getElementById("demo").innerHTML = "Paragraph changed.";
+    alert("đây là việt");
+}
+
+function showMessage(){
+    alert('Xin Chào !')
+}
